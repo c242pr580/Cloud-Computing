9b1f1d2735e0f5292b57f96e4e6c86357d91b9d2
Muhammad Baharuddin Yusuf <baharudinplh2018@gmail.com>
2024-11-29 13:03:06 +0000

    add waitress

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
-    ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
     MODEL_URL = os.getenv('MODEL_URL')
     LOCAL_MODEL_PATH = os.getenv('LOCAL_MODEL_PATH')
     GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
     VERIFICATION_IMG_BUCKET = os.getenv('VERIFICATION_IMG_BUCKET')
+
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import sys
 from app import create_app
+from waitress import serve
 
 sys.stdout.reconfigure(encoding='utf-8')
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    print('Running on http://'+app.config['HOST']+':'+app.config['PORT'])
+    serve(app, host=app.config['HOST'], port=app.config['PORT'])
